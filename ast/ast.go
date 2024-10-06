12d56faa597477595adeed0fc0de2bdb17a33fe1
Tim-PF <tim.pfops@gmail.com>
2024-10-06 14:54:38 +0200

    Basic skeleton of Parser and AST created

---
+++ ast/ast.go
@@ -0,0 +1,47 @@
+import  "github.com/Tim-PF/MonkeePK/token"
+
+package ast 
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+// Erstellt die root Node.
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token // the token .Let token
+	Name *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()  {}
+func (ls *LetStatement) TokenLiteral() string {return ls.Token.Literal}
+
+type Identifier struct {
+	Token token.Token // the token.Ident token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string {return i.Token.Literal }
---
+++ parser/parser.go
@@ -0,0 +1,33 @@
+package parser
+
+import (
+	 "github.com/Tim-PF/MonkeePK/lexer"
+	 "github.com/Tim.PF/MonkeePK/ast"
+	 "github.com/Tim.PF/MonkeePK/token"
+)
+// Exist of a lexer pointer current and next token
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken token.Token
+	peekToken token.Token
+}
+// Creates a new parser
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	//Read two tokens, so curToken and peekToken are both set
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+// Get the next token and current token to work with
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken() // in lexer not in parser. Not the same
+	}
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+	}
