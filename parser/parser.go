d7973bb934f35b3f1bb3b1fd6609a8d6961740f7
Tim-PF <tim.pfops@gmail.com>
2024-10-08 12:44:50 +0200

    Error Handling for Parsing Let Statements

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+"fmt"
 "github.com/Tim-PF/MonkeePK/lexer"
 "github.com/Tim-PF/MonkeePK/ast"
 "github.com/Tim-PF/MonkeePK/token"
@@ -11,10 +12,14 @@ type Parser struct {
 
 	curToken token.Token
 	peekToken token.Token
+
+	errors []string
 }
 // Creates a new parser
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{l: l,
+		         errors: []string{},
+				}
 
 	//Read two tokens, so curToken and peekToken are both set
 	p.nextToken()
@@ -85,6 +90,18 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.nextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
-	}
+}
+
+
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser ) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
+}
